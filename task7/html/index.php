8078a28aa14032bc1171170aa68a0892c977d2a0
mohammad5305 <mohammad.bajalal546@gmail.com>
2024-05-08 12:53:38 +0330

    task7: search form added

--- task7/html/index.php
+++ task7/html/index.php
@@ -5,18 +5,22 @@
  </head>
  <body>
   <?php
-    echo "<h1>Random Data</h1>";
+    echo "<h1><a href='/'>Random Data</a></h1>";
     $dbconn = pg_connect("host=database dbname=hugedata user=postgres password=password");
     $limit = isset($_GET['limit']) ? $_GET['limit'] : 20;
     $skip = isset($_GET['page']) ? $_GET['page']*$limit : 0;
     $columns = array("id_number","name", "phone_number");
-
+    echo "<form id='form_search'>";
     $conditions = array();
     foreach ($columns as $column) {
+        echo "<input type='text' name=\"$column\" class='search' placeholder=\"$column\">";
+
         if (isset($_GET[$column]) && ! empty($_GET[$column])) {
             array_push($conditions, sprintf("%s=%s", $column, pg_escape_literal($_GET[$column])));
         }
     }
+    echo "<button type='submit' onclick='on_click(self)'>Submit</button>";
+    echo "</form><br/>";
     $query = count($conditions) > 0 ? sprintf('SELECT * FROM people WHERE %s ORDER BY id_number LIMIT $1 OFFSET $2', join(" AND ", $conditions)) : 'SELECT * FROM people ORDER BY id_number LIMIT $1 OFFSET $2';
 
     $result = pg_query_params($dbconn, $query, array($limit, $skip));
@@ -25,8 +29,18 @@
         print_r($line);
         echo "<hr/>";
     }
-    echo sprintf("<a href='/?page=%s&limit=%s'><button>Next page</button></a>", (int)$_GET['page'] + 1, $limit);
-    echo sprintf("<a href='/?page=%s&limit=%s'><button>Previos page</button></a>", (int)$_GET['page'] - 1, $limit);
+
+    $params = "";
+
+    foreach ($_GET as $key => $value) {
+        if ($key != "page") {
+            $params .= "&$key=$value";
+        }
+    }
+    echo "<div class='btns'>";
+    echo sprintf("<a href='/?page=%s%s'><button>Previos page</button></a>", (int)$_GET['page'] - 1, $params);
+    echo sprintf("<a href='/?page=%s%s'><button>Next page</button></a>", (int)$_GET['page'] + 1, $params);
+    echo "</div>";
 
     // Free resultset
     pg_free_result($result);
